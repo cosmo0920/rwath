b2400e102b7a3ed10ac20c36c0a9f3b500df2fd8
cosmo0920 <cosmo_originstring@yahoo.co.jp>
2014-06-26 22:46:28 +0900

    update example

--- example/test-rwath.rb
+++ example/test-rwath.rb
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 require 'rwath'
 
-rwath = Rwath.new(delimitor: " ")
+conf = Rwath::Config.new
+conf.delimitor(char: ' ')
+conf.encode(input: :utf8, output: :utf8)
+rwath = Rwath.new(config: conf.setting)
 text = "ตัวอักษรไทยเป็นเรื่องยาก"
 puts result = rwath.split(text) # => ตัว อักษร ไทย เป็น เรื่อง ยาก
