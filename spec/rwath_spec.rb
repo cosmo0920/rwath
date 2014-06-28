7463d5a6f8ac2952a8f5845ce85d44a1a4bf57de
cosmo0920 <cosmo_originstring@yahoo.co.jp>
2014-06-29 01:31:20 +0900

    exclusion filters

--- spec/rwath_spec.rb
+++ spec/rwath_spec.rb
@@ -1,13 +1,18 @@
 # -*- coding: utf-8 -*-
 require "rwath"
 
+RSpec.configure do |c|
+  # declare an exclusion filter
+  c.filter_run_excluding :spec_ci => true
+end
+
 describe Rwath do
   let(:rwath) { Rwath.new }
   subject { rwath }
   it { should respond_to :split }
 
   describe "#split" do
-    context "when nothing specify" do
+    context "when nothing specify", :spec_ci => false do
       subject { rwath.split('ตัวอักษรไทยเป็นเรื่องยาก') }
       it "output with TIS-620 encoding by default" do
         should eq '฀ัวอั฀ษรเ฀ยเ฀เ฀เรืเอ฀ยา฀'
@@ -21,9 +26,17 @@ describe Rwath do
         config.delimitor(char: space)
         config.encode(input: 'u', output: 'u')
       end
+      let(:thai_characters) { 'ตัวอักษรไทยเป็นเรื่องยาก' }
       let (:rwath_with_delimitor) { Rwath.new(config: setting) }
-      subject { rwath_with_delimitor.split('ตัวอักษรไทยเป็นเรื่องยาก') }
-      it { should eq 'ตัว อักษร ไทย เป็น เรื่อง ยาก'}
+
+      context "local development", :spec_ci => false do
+        subject { rwath_with_delimitor.split(thai_characters) }
+        it { should eq 'ตัว อักษร ไทย เป็น เรื่อง ยาก'}
+      end
+      context "CI development", :spec_ci => true do
+        subject { rwath_with_delimitor.split(thai_characters) }
+        it { should include ' ' }
+      end
     end
   end
 end
@@ -31,10 +44,20 @@ end
 describe Rwath::Process do
   let(:command) { 'swath -u u,u' }
   let(:process) { Rwath::Process.new(command) }
+  let(:thai_characters) { 'ตัวอักษรไทยเป็นเรื่องยาก' }
   subject { process }
   it { should respond_to :split_exec }
-  describe "#split_exec" do
-    subject { process.split_exec('ตัวอักษรไทยเป็นเรื่องยาก') }
-    it { should eq 'ตัว|อักษร|ไทย|เป็น|เรื่อง|ยาก'}
+
+  context "local development", :spec_ci => false do
+    describe "#split_exec" do
+      subject { process.split_exec(thai_characters) }
+      it { should eq 'ตัว|อักษร|ไทย|เป็น|เรื่อง|ยาก'}
+    end
+  end
+  context "CI development", :spec_ci => true do
+    describe "#split_exec" do
+      subject { process.split_exec(thai_characters) }
+      it { should include '|' }
+    end
   end
 end
